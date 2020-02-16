c18277096a1a2567f9f3ab70b6b70861318d64cc
ekcs0915 <ekcs0915@gmail.com>
2020-02-15 19:28:53 -0800

    created a route between two points with a marker at the starting point

--- src/DisplayMapClass.js
+++ src/DisplayMapClass.js
@@ -3,38 +3,42 @@ import {platform} from "../config/platform";
 
 export class DisplayMapClass extends React.Component {
   mapRef = React.createRef();
-
   state = {
-    // The map instance to use during cleanup
     map: null
   };
+
   componentDidMount() {
     const defaultLayers = platform.createDefaultLayers();
-    // Create an instance of the map
+
     const map = new window.H.Map(
       this.mapRef.current,
       defaultLayers.vector.normal.map,
       {
-        // This map is centered over Europe
-	center:{lat: 37.7874102, lng:-122.3974609},
-        zoom: 17,
+        center: { lat: 50, lng: 5 },
+        zoom: 4,
         pixelRatio: window.devicePixelRatio || 1
       }
     );
+
+    // MapEvents enables the event system
+    // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
+    // This variable is unused and is present for explanatory purposes
+    const behavior = new window.H.mapevents.Behavior(new window.H.mapevents.MapEvents(map));
+
+    // Create the default UI components to allow the user to interact with them
+    // This variable is unused
+    const ui = window.H.ui.UI.createDefault(map, defaultLayers);
 
     this.setState({ map });
   }
 
   componentWillUnmount() {
-    // Cleanup after the map to avoid memory leaks when this component exits the page
     this.state.map.dispose();
   }
 
   render() {
-    return (
-      // Set a height on the map so it will display
-      <div ref={this.mapRef} style={{ height: "500px" }} />
-    );
+    return <div ref={this.mapRef} style={{ height: "500px" }} />;
   }
 }
+
 
--- src/components/DisplayMapClass.js
+++ src/components/DisplayMapClass.js
@@ -1,42 +1,92 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {platform} from "../config/platform";
-import './DisplayMapClass.css'
 
-class DisplayMapClass extends Component {
+class DisplayMapClass extends React.Component {
   mapRef = React.createRef();
-
   state = {
-    // The map instance to use during cleanup
     map: null
   };
+
   componentDidMount() {
     const defaultLayers = platform.createDefaultLayers();
-    // Create an instance of the map
+
     const map = new window.H.Map(
       this.mapRef.current,
       defaultLayers.vector.normal.map,
       {
-        // This map is centered over Europe
-	center:{lat: 37.7874102, lng:-122.3974609},
-        zoom: 17.7,  //17.7
-        pixelRatio: window.devicePixelRatio || 1,
+        center: { lat: 52.51, lng: 13.4 },
+        zoom: 10,
+        pixelRatio: window.devicePixelRatio || 1
       }
     );
+
+    const routingParameters = {
+      "mode": "fastest;car",
+      "waypoint0": "geo!37.788555,-122.3965872",
+      "waypoint1": "geo!37.786796,-122.398276",
+      "representation": "display"
+    }
+
+    let onResult = function(result) {
+      let route
+      let routeShape
+      let startPoint
+      let endPoint
+      let linestring
+      
+      if(result.response.route) {
+        route = result.response.route[0]
+        routeShape = route.shape
+        linestring = new window.H.geo.LineString()
+        routeShape.forEach(function(point) {
+          let parts = point.split(',')
+          linestring.pushLatLngAlt(parts[0], parts[1])
+        })
+
+        startPoint = route.waypoint[0].mappedPosition
+        endPoint = route.waypoint[1].mappedPosition
+
+        let routeLine = new window.H.map.Polyline(linestring, {
+          style: {strokeColor: "red", lineWidth: 5}
+        })
+
+        let startMarker = new window.H.map.Marker({
+          lat: startPoint.latitude,
+          lng: startPoint.longitude
+        })
+
+        let endMarker = new window.H.map.Marker({
+          lat: endPoint.latitude,
+          lng: endPoint.longitude
+        })
+
+        map.addObjects([routeLine, startMarker])
+
+        map.getViewModel().setLookAtData({bounds: routeLine.getBoundingBox()})
+      }
+    }
+
+    let router = platform.getRoutingService()
+
+    router.calculateRoute(routingParameters, onResult, function(err){
+      alert(err.message)
+    })
+
+    const behavior = new window.H.mapevents.Behavior(new window.H.mapevents.MapEvents(map));
+
+    const ui = window.H.ui.UI.createDefault(map, defaultLayers);
 
     this.setState({ map });
   }
 
   componentWillUnmount() {
-    // Cleanup after the map to avoid memory leaks when this component exits the page
     this.state.map.dispose();
   }
 
   render() {
-	  console.log(window.H)
-    return (
-        <div ref={this.mapRef} className='Map' />
-    );
+    return <div ref={this.mapRef} style={{ height: "500px" }} />;
   }
 }
+
 
 export default DisplayMapClass
